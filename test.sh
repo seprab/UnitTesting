a0f02d804ad035f9478feda3cd6e67a7a79c4576
Sergio Prada <sergioeprada@hotmail.com>
2022-07-27 11:08:19 -0500

    Cleaned the project and updated some information so anyone can set it up easily

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
-"C:\Program Files\Unity\Hub\Editor\2020.2.3f1\Editor\Unity.exe" \
+"C:\Program Files\Unity\Hub\Editor\2021.3.37f1\Editor\Unity.exe" \
 -batchmode \
--projectPath "C:\Users\sergi\Documents\TicketsTemp\1040714\UnitTesting" \
+-projectPath "D:\Tickets\xxxxxx\UnitTesting" \
 -runTests \
 -logFile log.txt \
 –quit \
